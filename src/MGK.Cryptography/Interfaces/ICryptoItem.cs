fed87e1913ad75c6875808c41c09ad2acb46eaa5
mkatz-zenfolio <mkatz@zenfolio.com>
2020-04-03 21:11:30 -0300

    Initial commit
    Initial commit

---
+++ src/MGK.Cryptography/Interfaces/ICryptoItem.cs
@@ -0,0 +1,23 @@
+﻿namespace MGK.Cryptography
+{
+	/// <summary>
+	/// Allow the implementation of an encrypted object.
+	/// </summary>
+	public interface ICryptoItem
+	{
+		/// <summary>
+		/// The encrypted value.
+		/// </summary>
+		byte[] Value { get; }
+
+		/// <summary>
+		/// The public encryption key.
+		/// </summary>
+		byte[] Key { get; }
+
+		/// <summary>
+		/// The initialization vector.
+		/// </summary>
+		byte[] InitializationVector { get; }
+	}
+}
